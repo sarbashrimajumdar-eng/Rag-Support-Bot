966575462303356a6e6e0878ba3fe7677525c462
sarbashrimajumdar-eng <sarbashrimajumdar@gmail.com>
2026-05-26 12:43:05 +0530

    Update rag_backend.py

--- rag_backend.py
+++ rag_backend.py
@@ -3,7 +3,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
